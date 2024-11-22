2764e8aabfe01d6511946f965976eeefe9c78872
zhaoyq0429 <zhaoyq0429@gmail.com>
2024-11-22 12:03:20 -0800

    Jest_form_components

--- components/form/CustomSelect.js
+++ components/form/CustomSelect.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function CustomSelect({ label, id, values }) {
   return (
     <>
--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/$1'
-    }
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1'
+  },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest'
   }
+}
